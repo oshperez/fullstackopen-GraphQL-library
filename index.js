b9d8a1a62f8c0d687d07280215516a7a7448ab64
Osmani <osmanihernandezperez@gmail.com>
2021-11-05 11:49:20 -0400

    Implement allBooks query - Ex. 8.2

--- index.js
+++ index.js
@@ -54,9 +54,18 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+
+  }
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book!]
   }
 `
 const resolvers = {
@@ -75,7 +84,8 @@ const resolvers = {
       }, [])
 
       return authors.length
-    }
+    },
+    allBooks: () => books
   }
 }
 
