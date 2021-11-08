5659e0b058c4bd84a1dac8c598543c6fe9dc3489
Osmani <osmanihernandezperez@gmail.com>
2021-11-08 10:46:06 -0500

    Modify resolver for allAuthors query

--- index.js
+++ index.js
@@ -4,6 +4,32 @@ const {
 } = require("apollo-server-core")
 const { v1: uuid } = require("uuid")
 
+let authors = [
+  {
+    name: "Robert Martin",
+    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
+    born: 1952,
+  },
+  {
+    name: "Martin Fowler",
+    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
+    born: 1963,
+  },
+  {
+    name: "Fyodor Dostoevsky",
+    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
+    born: 1821,
+  },
+  {
+    name: "Joshua Kerievsky", // birthyear not known
+    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
+  },
+  {
+    name: "Sandi Metz", // birthyear not known
+    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
+  },
+]
+
 let books = [
   {
     title: "Clean Code",
@@ -120,26 +146,15 @@ const resolvers = {
       return booksCopy
     },
     allAuthors: () => {
-      return books
-        .map((book) => book.author)
-        .reduce((allAuthors, currentAuthor) => {
-          let duplicatedAuthor = allAuthors.find(
-            (author) => author.name === currentAuthor
-          )
-          if (duplicatedAuthor) {
-            allAuthors = allAuthors.map((author) =>
-              author.name === duplicatedAuthor.name
-                ? {
-                    ...duplicatedAuthor,
-                    bookCount: (duplicatedAuthor.bookCount += 1),
-                  }
-                : author
-            )
-          } else {
-            allAuthors = [...allAuthors, { name: currentAuthor, bookCount: 1 }]
+      return authors.map((author) => {
+        let bookCount = 0
+        books.forEach((book) => {
+          if (book.author === author.name) {
+            bookCount++
           }
-          return allAuthors
-        }, [])
+        })
+        return { ...author, bookCount }
+      })
     },
   },
 
